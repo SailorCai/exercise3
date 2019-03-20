c7d2abc8137cbfe39a48bd4f86ea599dbfcd8742
蔡胜龙 <1187023819@qq.com>
2019-03-20 12:29:07 +0800

    接入travis CI 22222

--- karma.conf.js
+++ karma.conf.js
@@ -62,7 +62,7 @@ module.exports = function(config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless', 
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox'],
       }
     },
 
